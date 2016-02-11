30921c0a47fa4cf6afea0d12439192c6654f178b
Kris McCann <kris@8pi.ca>
2016-02-10 21:12:40 -0400

    js comments

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,7 @@
+// iife to isolate scope, pass context and jQ
 (function(window, $) {
 	$(document).ready(function() {
+		// add opacity 1 to anything with fade-in class, css will take care of the rest
 	    $('.fade-in').css('opacity', '1');
 	});
 })(window, jQuery);
